b7409d4a04987e0f1a76ae289ed34961635bc4a8
Amir Muhammad Hakim <amir.muh.hakim@gmail.com>
2021-03-01 00:42:35 +0700

    ⚡️ Enabling react-native-screens

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { enableScreens } from 'react-native-screens';
+
+enableScreens();
 
 export default function App() {
   return (
