fc77341a91443c0b8f82c0db5784d6fb39e2f130
Amir Muhammad Hakim <amir.muh.hakim@gmail.com>
2021-03-06 21:58:56 +0700

    ✨ Finishing doctor profile screen layout

--- screens/DoctorProfileScreen.tsx
+++ screens/DoctorProfileScreen.tsx
@@ -1,14 +1,74 @@
+import { useRoute } from '@react-navigation/core';
 import * as React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 
-const DoctorProfileScreen: React.FC = () => (
-  <View style={styles.screen}>
-    <Text>DoctorProfileScreen</Text>
-  </View>
-);
+import AppButton from '../components/atoms/AppButton';
+import DoctorProfileItem from '../components/molecules/DoctorProfileItem';
+import ProfileWithPhoto from '../components/molecules/ProfileWithPhoto';
+import Header from '../components/molecules/header/Header';
+import Colors from '../constants/colors';
+import { DoctorProfileScreenRouteProp } from '../global-types/navigation';
+import withStatusBar from '../hoc/withStatusBar';
+
+const DoctorProfileScreen: React.FC = () => {
+  const { params } = useRoute<DoctorProfileScreenRouteProp>();
+  return (
+    <View style={styles.screen}>
+      <Header title="Profile Dokter" type="flat" />
+      <ScrollView style={styles.body} showsVerticalScrollIndicator={false}>
+        <ProfileWithPhoto
+          style={styles.profileWithPhoto}
+          name={params.doctor.name}
+          description={params.doctor.specialist}
+          photoUrl={params.doctor.photoUrl}
+          onRemoveAvatar={() => null}
+        />
+        <DoctorProfileItem
+          style={styles.profileItem}
+          label="Alumnus"
+          data={params.doctor.almamater}
+        />
+        <DoctorProfileItem
+          style={styles.profileItem}
+          label="Tempat Praktik"
+          data={params.doctor.workplace}
+        />
+        <DoctorProfileItem
+          style={styles.profileItem}
+          label="No. STR"
+          data={params.doctor.credentialID}
+        />
+        <AppButton
+          style={styles.button}
+          title="Start Consultation"
+          color="accent"
+          onPress={() => null}
+        />
+      </ScrollView>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  screen: {},
+  screen: {
+    flex: 1,
+  },
+  body: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  profileWithPhoto: {
+    marginTop: 10,
+    marginBottom: 26,
+  },
+  profileItem: {
+    marginBottom: 16,
+  },
+  button: {
+    marginTop: 7,
+    marginBottom: 40,
+    marginHorizontal: 24,
+  },
 });
 
-export default DoctorProfileScreen;
+export default withStatusBar(DoctorProfileScreen, 'dark', Colors.White);
